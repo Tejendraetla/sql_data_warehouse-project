9d4f0307debf63bec358e0350d76a31bffcb279d
Tejendraetla <94368807+Tejendraetla@users.noreply.github.com>
2025-09-08 10:00:06 -0700

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,3 +1,17 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+
+
+
 use master;
 create database datawarehouse;
 use datawarehouse;
